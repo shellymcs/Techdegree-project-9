7677b3b98e5ae3233ad31a5b0246ff23aea73c1e
shellymcs <61569144+shellymcs@users.noreply.github.com>
2020-05-12 12:25:22 +1200

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 
+
+// Scrollspy 
 $('body').scrollspy({ target: '#navbarTogglerDemo02' })
-    
+
+
+//Dont fade in tagline until page/background photo loaded
+$(window).bind("load", function() {
+    $(document.getElementsByClassName('headline')).removeClass("hidden");
+ });  
